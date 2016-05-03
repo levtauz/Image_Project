ee06cff0ae8dd2ab07377c700482bc7e5c058459
Harrison Wang <h.wang94@berkeley.edu>
2016-05-03 13:28:32 -0700

    fix

--- src/aprs.py
+++ src/aprs.py
@@ -505,11 +505,11 @@ def APRS_rcv(Qin, Qout, Qtext, modem):
 
 
      while(1):
-        tmp = Qin.get()
+         tmp = Qin.get()
         Qout.put(tmp)
         packets  = modem.processBuffer(tmp)
-	for ax in packets:
-             Qtext.put(ax)
+        for ax in packets:
+            Qtext.put(ax)
 
 
 
